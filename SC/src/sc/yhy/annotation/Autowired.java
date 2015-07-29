6d7e99f209c71c35f9e64d0f2311deafd27dd20a
yhy <snxamdf@126.com>
2015-07-29 17:46:02 +0800

    YHY 修改注解类型

--- SC/src/sc/yhy/annotation/Autowired.java
+++ SC/src/sc/yhy/annotation/Autowired.java
@@ -5,12 +5,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 /**
  * @time 2015-07-29
  * @author YHY
  *
  */
-@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Target({ ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Autowired {
