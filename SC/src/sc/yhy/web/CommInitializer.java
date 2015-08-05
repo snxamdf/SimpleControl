9bb956ffa3befeb37d2ca2733af7f5747d92dddf
yhy <snxamdf@126.com>
2015-08-05 18:22:09 +0800

    YHY update

--- SC/src/sc/yhy/web/CommInitializer.java
+++ SC/src/sc/yhy/web/CommInitializer.java
@@ -8,18 +8,27 @@ import sc.yhy.listener.AnnotationListener;
 
 @Order("" + Integer.MAX_VALUE)
 class CommInitializer implements WebApplicationInitializer {
+	String[] mapping = { "*.jpg", "*.jpeg", "*.swf", "*.png", "*.gif", "*.ico", "*.js", "*.css", "*.xml", "*.shtml", "*.html", "*.htm" };
+
 	/**
 	 * 框架基本配置加载
 	 */
 	@Override
 	public void init(ServletContext servletContext) throws ServletException {
-
+		String serverInfo = servletContext.getServerInfo();
+		servletContext.log("当前运行WEB容器---" + serverInfo);
 		// 添加主监听
 		servletContext.addListener(AnnotationListener.class);
 		// 添加主servlet
 		servletContext.addServlet("annotationServlet", "sc.yhy.servlet.AnnotationServlet").addMapping("/");
-		// 配置静态文件默认tomcat default
-		servletContext.addServlet("default", "org.apache.catalina.servlets.DefaultServlet").addMapping("*.jpg", "*.png", "*.gif", "*.ico", "*.js", "*.css", "*.xml", "*.shtml", "*.html", "*.htm");
+		if (serverInfo.toLowerCase().indexOf("tomcat") != -1) {
+			// 配置静态文件默认tomcat default
+			servletContext.addServlet("default", "org.apache.catalina.servlets.DefaultServlet").addMapping(mapping);
+		} else if (serverInfo.toLowerCase().indexOf("weblogic") != -1) {
+		} else if (serverInfo.toLowerCase().indexOf("jboss") != -1) {
+		} else if (serverInfo.toLowerCase().indexOf("websphere") != -1) {
+		} else if (serverInfo.toLowerCase().indexOf("resin") != -1) {
+		}
 		// 其它..待添加
 	}
 
